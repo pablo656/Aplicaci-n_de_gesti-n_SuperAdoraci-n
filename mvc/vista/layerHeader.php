74227f7499f825671d45efd953b2b5461505bec1
AlejandroGB <alexgarciabermudez2006@gmail.com>
2026-03-04 21:29:02 +0100

    Header con sesión y perfil

--- mvc/vista/layerHeader.php
+++ mvc/vista/layerHeader.php
@@ -1,11 +1,31 @@
-<?php
-// Se convierte este archivo en un fragmento de cabecera (sin <html>, <head> ni <body>)
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/header_style.css">
+</head>
+<body>
 <header>
-    <img src="/git_develover/mvc/vista/imagenes/Logo.jpeg" alt="Logo">
+    <img src="imagenes/Logo.jpeg" alt="Logo">
 </header>
+
 <nav>
-    <a href="#">Inicio</a>
-    <a href="#">Catalogo</a>
-    <a href="#">Pedidos</a>
+    <div>
+        <a href="#">Inicio</a>
+        <a href="#">Catalogo</a>
+        <a href="#">Pedidos</a>
+    </div>
+    <div>
+        <?php
+        if(isset($_SESSION["id"]) && isset($_SESSION["nombre"])&&isset($_SESSION["email"])){?>
+            <a href="#">Perfil</a>
+            <?php
+        }else{?>
+        <a href="#">Log in</a>
+        <a href="#">Sing in</a>
+        <?php
+        }?>
+    </div>
 </nav>
